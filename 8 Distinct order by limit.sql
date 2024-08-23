558759f6ed9c7967949c3ccf16d5fc60f53179dc
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2024-08-23 11:09:08 +0530

    check and distinct file added

---
+++ 8 Distinct order by limit.sql
@@ -0,0 +1,38 @@
+show databases;
+use db3;
+
+-- distinct, order by, limit
+create table tbldistinct (
+id int not null auto_increment primary key,
+fname varchar(20),
+lname varchar(20)
+);
+
+select * from tbldistinct;
+
+insert into tbldistinct(fname,lname) values('sam','samson'),
+('sam','samson'),('dad','dadso'),('gas','gases'),('pas','pasm'),('fassam','fassamson'),('sam','samson');
+
+select *from tbldistinct;
+
+select distinct(fname) from tbldistinct;
+select count(distinct(fname)) as namecount from tbldistinct;
+
+
+create table orderby (
+id int not null auto_increment primary key,
+fname varchar(20) not null,
+lname varchar(20) not null
+);
+
+select * from orderby;
+
+insert into orderby(fname,lname) values('a','aa'),('q','qq'),('w','ww'),('r','rr'),('y','yy'),
+('e','ee'),('h','hh'),('m','mm'),('l','ll'),('i','ii');
+
+select * from orderby;
+
+select * from orderby order by fname;
+select * from orderby order by fname desc;
+
+select * from orderby limit 5;
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-sql
+SQL practice:- 
+1 create database
+2 show database
+3 select statements
+4 CRUD operations:-
+    1 create table
+    2 select/read data from table.
+    3 update the record/s
+    4 delete any record from the table.
+5 null, not null, auto_increment, default constraint
+5 DDL and DML
+6 primary key and unique key
+7 foreign key
+8 check constraint
