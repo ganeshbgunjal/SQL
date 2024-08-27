900332758d837db826bb0bce0c05a67df16d1ea5
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2024-08-27 09:27:48 +0530

    order of execution file added

---
+++ 9 order of execution in sql.sql
@@ -0,0 +1,12 @@
+show databases;
+use db3;
+
+-- order of execution in sql statements
+select * from table_name where fname like 'x%';    -- random code..
+
+-- this is execution of sql code. 
+-- G - group by
+-- H - having
+-- O - order by
+-- L - limit
+
